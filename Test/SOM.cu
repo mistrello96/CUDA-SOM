760d1bef72d338b9a77db9296bc95db4e5e411bf
Matteo Mistri <matteo.mistri1996@gmail.com>
2018-03-02 16:41:22 +0100

    Working matrix passage, working distance for bmu calculation

--- Test/SOM.cu
+++ Test/SOM.cu
@@ -3,10 +3,22 @@
 #include <cuda_runtime.h>
 
 __global__ void
-test_kernel(float* matrix, int numElements)
+test_kernel(float* matrix, int numberNeuron, float* sample, float* distance, int sampleLength)
 {
+	//dovrei avere blocchetti da 14 valori, a ogni blocchetto corrisponde una "distanza"
+	//sample in input. Chi la calcola? Chi la salva?
+	int index = (threadIdx.x + blockIdx.x * blockDim.x);
+	if (index < numberNeuron)
+	{
+		int matrixindex = index * 14;
+		float tmp = 0;
+		for(int i = 0; i < sampleLength; i++)
+		{
+			tmp = tmp + matrix[matrixindex+i] - sample[i];
+		}
 
-	matrix[blockIdx.x] = matrix[blockIdx.x] * 2;
+		distance[index] = distance[index] + tmp;
+	}
 }
 
 int
@@ -14,30 +26,43 @@ main(void)
 {
 
     int numberElements = 14;
-    int numberRows = 10;
-    int numberColoumns = 10;
+    int numberRows = 2;
+    int numberColoumns = 2;
+    int numberNeuron = numberRows * numberColoumns;
     int totalLength = numberRows * numberColoumns * numberElements;
 
     float *hostMatrix = (float *)malloc(sizeof(float) * totalLength);
+    float *hostSample = (float *)malloc(sizeof(float) * numberElements);
+    float *hostDistance = (float *) malloc(sizeof(float) * numberNeuron);
+    //random initialization
     for(int i = 0; i < totalLength; i++){
     	hostMatrix[i] = i;
-    	std::cout << hostMatrix[i] << std::endl;
+    }
+    for(int i = 0; i < numberNeuron; i++){
+    	hostDistance[i] = 0;
+    }
+    for(int i = 0; i < numberElements; i++){
+    	hostSample[i] = i+1;
     }
 
     float *deviceMatrix;
+    float *deviceSample;
+    float *deviceDistance;
     cudaMalloc((void **)&deviceMatrix, sizeof(float) * totalLength);
-
+    cudaMalloc((void**)&deviceSample, sizeof(float) * numberElements);
+    cudaMalloc((void**)&deviceDistance, sizeof(float) * numberNeuron);
 	//copy to device memory
 	cudaMemcpy(deviceMatrix, hostMatrix, sizeof(float) * totalLength, cudaMemcpyHostToDevice);
-
-	test_kernel<<<totalLength,1>>>(deviceMatrix, totalLength);
+	cudaMemcpy(deviceSample, hostSample, sizeof(float) * numberElements, cudaMemcpyHostToDevice);
+	cudaMemcpy(deviceDistance, hostDistance, sizeof(float) * numberNeuron, cudaMemcpyHostToDevice);	
+	test_kernel<<<100,100>>>(deviceMatrix, numberNeuron, deviceSample, deviceDistance, numberElements);
 
     cudaDeviceSynchronize();
 
-	cudaMemcpy(hostMatrix, deviceMatrix, sizeof(float) * totalLength, cudaMemcpyDeviceToHost);
+	cudaMemcpy(hostDistance, deviceDistance, sizeof(float) * numberNeuron, cudaMemcpyDeviceToHost);
 
-    for(int i = 0; i < totalLength; i++){
-    	std::cout << hostMatrix[i] << std::endl;
+    for(int i = 0; i < numberNeuron; i++){
+    	std::cout << hostDistance[i] << std::endl;
     }
 
     cudaFree(deviceMatrix);
